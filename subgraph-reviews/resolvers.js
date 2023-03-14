7140655c71c484891e8e4f318254a65ac55fcde1
boitshoko-glucode <boitshoko@glucode.com>
2023-03-14 15:56:15 +0200

    Fed 1

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -4,6 +4,19 @@ const resolvers = {
       return dataSources.reviewsAPI.getLatestReviews();
     }
   },
+  Location: {
+    overallRating: ({id}, _, {dataSources}) => {
+      return dataSources.reviewsAPI.getOverallRatingForLocation(id);
+    },
+    reviewsForLocation: ({id}, _, {dataSources}) => {
+      return dataSources.reviewsAPI.getReviewsForLocation(id);
+     },
+  },
+  Review: {
+    location: ({locationId}) => {
+      return {id: locationId};
+    },
+  },
   Mutation: {
     submitReview: (_, {locationReview}, {dataSources}) => {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
